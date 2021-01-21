ca92b3a3bdb8ddb66d6fbc0446e02249acb03bbb
Priya Garg <gupta.ria100@gmail.com>
2021-01-20 18:14:38 -0600

    making connection for jawsDB and for local

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,19 @@
-var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "priyapassword",
-  database: "burgers_db"
-});
+var mysql = require('mysql');
+if (process.env.JAWSDB_URL) {
+    // Database is JawsDB on Heroku
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    // Database is local
+    var connection = mysql.createConnection({
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "priyapassword",
+      database: "burgers_db"
+    })
+};
+
 
 // Make connection.
 connection.connect(function(err) {
@@ -19,3 +26,9 @@ connection.connect(function(err) {
 
 // Export connection for our ORM to use.
 module.exports = connection;
+
+// process.env.DB_USERNAME
+// process.env.DB_PASSWORD
+
+// DB_USERNAME=root,
+// DB_PASSWORD=priyapassword
